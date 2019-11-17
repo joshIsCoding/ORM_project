ce2ec12281fe469ee547d93476eb6b20cc34c159
joshIsCoding <yes_you_do@hotmail.com>
2019-11-17 18:13:07 +0000

    Add Reply::find_by_author_id and ::find_by_subject_question_id queries as methods

--- data_classes/reply.rb
+++ data_classes/reply.rb
@@ -23,5 +23,31 @@ class Reply
       Reply.new(reply.first) # query returns array of hashes
    end
 
+   def self.find_by_author_id(author_id)
+      author_replies = QuestionsDatabase.instance.execute(<<-SQL, author_id)
+         SELECT
+            *
+         FROM
+            replies
+         WHERE
+            author_id = ?
+      SQL
+      return nil unless author_replies.length > 0
+      author_replies.map {|auth_reply| Reply.new(auth_reply) } 
+   end
+
+   def self.find_by_subject_question_id(subject_question_id)
+      question_replies = QuestionsDatabase.instance.execute(<<-SQL, subject_question_id)
+         SELECT
+            *
+         FROM
+            replies
+         WHERE
+            subject_question_id = ?
+      SQL
+      return nil unless question_replies.length > 0
+      question_replies.map {|qu_reply| Reply.new(qu_reply) } 
+   end
+
    
 end
