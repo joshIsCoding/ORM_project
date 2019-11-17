3891dbc5db259bc3120a2aa797b7078bc325a6f5
joshIsCoding <yes_you_do@hotmail.com>
2019-11-17 15:33:45 +0000

    Write User::find_by_id method

--- data_classes/user.rb
+++ data_classes/user.rb
@@ -1,9 +1,24 @@
+require_relative "../QuestionsDatabase.rb"
+
 class User
    attr_accessor :id, :fname, :lname
    def initialize(options)
       @id = options["id"]
       @fname = options["fname"]
       @lname = options["lname"]
+   end
+
+   def self.find_by_id(id)
+      user = QuestionsDatabase.instance.execute(<<-SQL, id)
+         SELECT
+            *
+         FROM
+            users
+         WHERE
+            id = ?
+      SQL
+      return nil unless user.length > 0
+      User.new(user.first) # query returns array of hashes
    end
 
    
