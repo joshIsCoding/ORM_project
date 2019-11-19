54ff1723b7d4be8dbf94841bc188a6ae1668fd2b
joshIsCoding <yes_you_do@hotmail.com>
2019-11-19 13:01:55 +0000

    Add QuestionLike::likers_for_question_id, ::num_likes_for_question_id and ::liked_questions_for_user_id queries

--- data_classes/question_like.rb
+++ data_classes/question_like.rb
@@ -19,5 +19,49 @@ class QuestionLike
       QuestionLike.new(question_like.first) # query returns array of hashes
    end
 
+   def self.likers_for_question_id(question_id)
+      likers = QuestionsDatabase.instance.execute(<<-SQL, question_id)
+         SELECT
+            users.id, fname, lname
+         FROM
+            question_likes
+         INNER JOIN
+            users ON users.id = question_likes.user_id
+         WHERE
+            question_id = ?
+
+      SQL
+      likers.map { |liker| User.new(liker) }
+   end
+
+   def self.num_likes_for_question_id(question_id)
+      count = QuestionsDatabase.instance.execute(<<-SQL, question_id)
+         SELECT
+            COUNT(user_id) AS like_count
+         FROM
+            question_likes
+         WHERE
+            question_id = ?
+      SQL
+      count.first["like_count"]
+   end
+   
+
+   def self.liked_questions_for_user_id(user_id)
+      liked_questions = QuestionsDatabase.instance.execute(<<-SQL, user_id)
+         SELECT
+            questions.id, title, body, author_id
+         FROM
+            question_likes
+         INNER JOIN
+            questions ON questions.id = question_likes.question_id
+         WHERE
+            user_id = ?
+
+      SQL
+      liked_questions.map { |liked_qu| Question.new(liked_qu) }
+   end
+
+
    
 end
